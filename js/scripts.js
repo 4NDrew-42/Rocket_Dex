f133a22a44d4b445587a94ce87558cd241dcbcb3
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-10-01 00:46:34 -0500

    text and slider in

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-document.querySelector("#pokedex-icon").addEventListener("load", function() {
-    const outerObject = this; // Reference to the <object> element
-    const scalingSolid = document.getElementById("scaling-solid"); // Reference to the scaling solid
+document.querySelector("#pokedex-icon").addEventListener("click", function() {
+    const outerObject = this;
+    const scalingSolid = document.getElementById("scaling-solid");
+    const pokemonLogo = document.querySelector('.pokemon-logo');
+    const boxElement = document.querySelector('.box');
+    
+    // Add the clicked class to apply the scaling effect to the pokedex-icon
+    outerObject.classList.add("clicked");
 
-    outerObject.addEventListener("click", function() {
-        // Add the clicked class to apply the scaling effect to the pokedex-icon
-        outerObject.classList.add("clicked");
+    setTimeout(() => {
+        scalingSolid.classList.add("clicked");
 
-        // Add a delay to then apply the clicked class to scalingSolid
-        setTimeout(() => {
-            scalingSolid.classList.add("clicked");
-        }, 1000);  // 1000ms or 1s, which is the time it takes for the pokedex-icon's scaling to finish
-    });
+        setTimeout(function() {
+            pokemonLogo.style.opacity = '1';
+            pokemonLogo.style.animation = 'bounceScale 1.3s forwards';
+
+            // After the bounce animation is complete, slide in the box and slider
+            setTimeout(function() {
+                boxElement.style.left = '35%'; // Initiating the slide-in via CSS transition
+            }, 2000); // Waiting for 2s after the logo animation starts
+
+        }, 2000);
+    }, 1000);
 });
 
 window.onload = function() {
