589d582d8b4b87f6da0118f790f4114c56fdcd96
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-09-30 20:03:42 -0500

    ios icon scaling
    ios icon properly scales into center of screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
 document.querySelector("#pokedex-icon").addEventListener("load", function() {
-    const svgDoc = this.contentDocument;
-    const outerObject = this; // Reference to the outer <object> element
+    const outerObject = this; // Reference to the <object> element
 
-    svgDoc.addEventListener("DOMContentLoaded", function() {
-        const pokedexIcon = svgDoc.getElementById("pokedexIcon");
-        if (pokedexIcon) {
-            pokedexIcon.addEventListener("click", function() {
-                // Add the clicked class to the outer <object> element to apply the scaling effect
-                outerObject.classList.toggle("clicked");
-                
-                setTimeout(() => {
-                    gsap.to(svgDoc.getElementById("topYellow"), {duration: 0.3, attr:{d: 'M0 0C0 0 5.37258 0 12 0H48C54.6274 0 60 0 60 0V29.6119H0V0Z'}});
-                    gsap.to(svgDoc.getElementById("bottomYellow"), {duration: 0.3, attr:{d: 'M0 30.3984H60V60.0103C60 60.0103 54.6274 60.0103 48 60.0103H12C5.37258 60.0103 0 60.0103 0 60.0103V30.3984Z'}});
-                }, 1000);
-            });
-        }
+    // Directly add a click event to the outerObject since it encapsulates the SVG
+    outerObject.addEventListener("click", function() {
+        // Add the clicked class to apply the scaling effect
+        outerObject.classList.add("clicked");
     });
 });
 
-
+// For the fade-in effect:
+window.onload = function() {
+    document.body.classList.add("is-loaded");
+};
