d0e5a3e5265c0e3820519d5702881d8232d97801
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-09-30 22:41:48 -0500

    1.1
    yellow background proper scaling from app click

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 document.querySelector("#pokedex-icon").addEventListener("load", function() {
     const outerObject = this; // Reference to the <object> element
+    const scalingSolid = document.getElementById("scaling-solid"); // Reference to the scaling solid
 
-    // Directly add a click event to the outerObject since it encapsulates the SVG
     outerObject.addEventListener("click", function() {
-        // Add the clicked class to apply the scaling effect
+        // Add the clicked class to apply the scaling effect to the pokedex-icon
         outerObject.classList.add("clicked");
+
+        // Add a delay to then apply the clicked class to scalingSolid
+        setTimeout(() => {
+            scalingSolid.classList.add("clicked");
+        }, 1000);  // 1000ms or 1s, which is the time it takes for the pokedex-icon's scaling to finish
     });
 });
 
-// For the fade-in effect:
 window.onload = function() {
     document.body.classList.add("is-loaded");
 };
